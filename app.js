9e449ce454e5e2194572c4857cc563779cf6ea13
imkuljeet <iamkkashyap@gmail.com>
2024-04-18 14:20:50 +0530

    Integrating the frontend with backend

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin : "*",
+  // methods : ["GET","POST"],
+  // credentials : true
+}));
+
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
--- views/signup.js
+++ views/signup.js
@@ -14,6 +14,8 @@ async function signup(e) {
       const response = await axios.post('http://localhost:3000/user/signup', signUpDetails);
   
       if (response.status === 201) {
+        alert('Successfully Signed Up');
+
         window.location.href = "./login.html";
       } else {
         throw new Error("Failed to create user. Please try again.");
@@ -26,6 +28,7 @@ async function signup(e) {
       errorDiv.style.color = 'red';
 
       if (err.response && err.response.status === 400) {
+        alert('User already exists, Please Login');
         errorDiv.textContent = err.response.data.message;
       } else {
         errorDiv.textContent = err.message || "An error occurred. Please try again.";
