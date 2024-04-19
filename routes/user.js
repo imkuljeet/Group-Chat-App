c07a15cc410d3286559f734e6359ec00d90efde8
imkuljeet <iamkkashyap@gmail.com>
2024-04-19 20:24:40 +0530

    Creating the ChatApp Backend

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user");
+const msgController = require('../controllers/message');
+const authorization = require('../middleware/auth');
 
 router.post('/signup', userController.signup);
 router.post('/login',userController.login);
+router.post('/message', authorization.authenticate, msgController.postMessage)
+
 
 module.exports = router;
